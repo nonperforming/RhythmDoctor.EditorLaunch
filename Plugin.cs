ca8a6ec95204fd709911acfe9a9e5f5a53836405
Non <43197300+nonperforming@users.noreply.github.com>
2025-05-10 23:10:53 +1200

    Cleanup logic

--- Plugin.cs
+++ Plugin.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using BepInEx;
 using BepInEx.Logging;
-using HarmonyLib;
 using RDLevelEditor;
 using UnityEngine;
 
@@ -10,42 +9,67 @@ namespace RhythmDoctor.EditorLaunch;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 [BepInProcess("Rhythm Doctor.exe")]
-public class Plugin : BaseUnityPlugin
+internal class Plugin : BaseUnityPlugin
 {
-  internal static new ManualLogSource Logger;
-  internal static readonly string EditorLaunchFile = Path.Join(Application.persistentDataPath, "editorlaunch.txt");
+  private static new ManualLogSource Logger;
+  private static readonly string EditorLaunchFile = Path.Join(Application.persistentDataPath, "editorlaunch.txt");
 
-  internal static string Level = null;
-  internal static bool RunOnce;
+  private static string Level;
 
   private void Awake()
   {
-    // Plugin startup logic
     Logger = base.Logger;
     Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
 
-    Harmony.CreateAndPatchAll(typeof(Patch), "non.editorlaunch");
+    RDStartup.Setup();
+    if (!LaunchedWithLevel()) return;
+    if (!SteamIntegration.initialized)
+    {
+      Logger.LogWarning("Not launched with Steam, relaunching");
+      File.WriteAllText(EditorLaunchFile, Level);
+      Application.OpenURL("steam://launch/774181");
+      Application.Quit();
+    }
+
+    scnCLS.customLevelPath = Level;
+    scnBase.GoToScene(nameof(scnEditor));
   }
 
-  internal static bool LaunchedWithLevel()
+  private static bool IsValidLevel(string path)
+  {
+    Logger.LogDebug($"Checking {path}");
+    if (Path.GetExtension(path) is ".rdlevel" or ".rdzip")
+    {
+      Logger.LogDebug("Extension check passed");
+      if (File.Exists(path))
+      {
+        Logger.LogDebug("Exist check passed");
+        return true;
+      }
+      Logger.LogDebug("Exist check failed");
+    }
+    else
+    {
+      Logger.LogDebug("Extension check failed");
+    }
+    
+    return false;
+  }
+  
+  private static bool LaunchedWithLevel()
   {
     if (File.Exists(EditorLaunchFile))
     {
-      Level = File.ReadAllText(EditorLaunchFile);
+      string path = File.ReadAllText(EditorLaunchFile);
       File.Delete(EditorLaunchFile);
-      Logger.LogInfo($"Editor launch file found: {Level}");
+      Logger.LogInfo($"Editor launch file found: {path}");
       
-      string extension = Path.GetExtension(Path.GetFileName(Level));
-      Logger.LogDebug($"Extension is {extension}");
-      if (extension == ".rdlevel" || extension == ".rdzip")
+      if (IsValidLevel(path))
       {
-        if (File.Exists(Level))
-        {
-          return true;
-        }
+        Level = path;
+        return true;
       }
-      Logger.LogInfo("Editor launch file is bogus");
-      Level = null;
+      Logger.LogError("Editor launch file is bogus");
     }
     else
     {
@@ -53,69 +77,21 @@ public class Plugin : BaseUnityPlugin
     }
 
     string[] args = Environment.GetCommandLineArgs();
-    if (args.Length == 1)
+    if (args.Length == 1 || (args.Length == 2 && args[1] == "--force-d3d11"))
     {
       Logger.LogInfo("No file passed");
       return false;
     }
 
-    foreach (string argument in Environment.GetCommandLineArgs())
+    foreach (string argument in args)
     {
-      string extension = Path.GetExtension(Path.GetFileName(argument));
-
-      Logger.LogDebug($"Extension of {argument} is {extension}");
-      if (extension == ".rdlevel" || extension == ".rdzip")
-      {
-        Logger.LogDebug("Extension check passed");
-        if (File.Exists(argument))
-        {
-          Logger.LogDebug("Exist check passed");
-          Level = argument;
-        }
-        else
-        {
-          Logger.LogDebug("Exist check failed");
-        }
-      }
-      else
-      {
-        Logger.LogDebug("Extension check failed");
-      }
-    }
-
-    if (Level.IsNullOrWhiteSpace())
-    {
-      Logger.LogInfo("Couldn't find a level");
-      return false;
-    }
-
-    Logger.LogInfo($"Launched with {Level}");
-    return true;
-  }
-
-  [HarmonyPatch(typeof(RDStartup))]
-  class Patch
-  {
-    [HarmonyPatch(nameof(RDStartup.Setup))]
-    [HarmonyPostfix]
-    static void PiggybackSetup()
-    {
-      if (RunOnce) return;
-      if (!LaunchedWithLevel()) return;
+      if (!IsValidLevel(argument)) continue;
       
-      RunOnce = true;
-      
-      // Check if Steam is open
-      if (!SteamIntegration.initialized)
-      {
-        Logger.LogWarning("Not launched with Steam, relaunching");
-        File.WriteAllText(EditorLaunchFile, Level);
-        Application.OpenURL("steam://launch/774181");
-        Application.Quit();
-      }
-
-      scnCLS.customLevelPath = Level;
-      scnBase.GoToScene(nameof(scnEditor));
+      Level = argument;
+      return true;
     }
+
+    Logger.LogInfo("Could not find level");
+    return false;
   }
 }
