7e2387afb3bd924ce8a115e226d58aa83fe904dd
Non <43197300+nonperforming@users.noreply.github.com>
2025-05-30 13:26:10 +1200

    Cleanup macOS event loop

--- Launcher/src/macos.rs
+++ Launcher/src/macos.rs
@@ -1,6 +1,6 @@
 //! Handle .rdlevel/.rdzip/.zip files opened in macOS using an event loop
 
-use log::{debug, info, warn};
+use log::{debug, info, trace, warn};
 use tao::event::Event;
 use tao::event_loop::ControlFlow;
 use tao::event_loop::EventLoop;
@@ -12,32 +12,39 @@ use crate::open_rhythm_doctor_with_level;
 pub fn event_loop(with_steam: bool) -> ! {
     debug!("Creating event loop to listen for file");
 
-    let event_loop = EventLoop::new();
-    let mut empty_polls: u8 = 0;
-
-    event_loop.run(move |event, _, control_flow| {
+    let mut exiting = false;
+    EventLoop::new().run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
+        if exiting {
+            // Do not process events when we're exiting.
+            // In theory we should be able to check `*control_flow == ControlFlow::Exit`,
+            //  as it is "sticky" but control_flow always seems to be set to ControlFlow::Wait
+            return;
+        }
+
+        trace!("Control flow: {:?}", control_flow);
+        trace!("Event: {:?}", event);
+
         if let Event::Opened { urls } = event {
-            info!("Opened event");
+            info!("Got Opened event");
             if let Some(level) = urls.first() {
                 open_rhythm_doctor_with_level(
                     with_steam,
                     &level.to_file_path().unwrap().display().to_string(),
                 );
                 *control_flow = ControlFlow::ExitWithCode(0);
+                exiting = true;
             } else {
                 warn!("No URL found in opened event");
+                *control_flow = ControlFlow::ExitWithCode(2);
+                exiting = true;
             }
         } else {
-            // #[allow(clippy::arithmetic_side_effects)] // blocked by https://github.com/rust-lang/rust/issues/15701
-            empty_polls += 1;
-            // == 50 as we do not want to open the config file multiple times
-            if empty_polls == 50 {
-                warn!("No events for 50 polls, opening configuration file and closing");
-                open_config_file().unwrap();
-                *control_flow = ControlFlow::ExitWithCode(1);
-            }
+            warn!("No Opened event, opening configuration file");
+            open_config_file().unwrap();
+            *control_flow = ControlFlow::ExitWithCode(1);
+            exiting = true;
         }
     });
 }
